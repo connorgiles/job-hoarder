33b52d774338a1f22a4fe1e054fa281278d4ffba
Connor Giles <connor@connorgiles.com>
2020-06-29 01:10:24 -0400

    removed incompatible libraries

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,3 @@ import './types';
 
 export { default as Greenhouse } from './greenhouse';
 export { default as Lever } from './lever';
-/*
-import Lever from './lever';
-import JazzAPI from './jazzAPI';
-import JazzScrape from './jazzScrape';
-import SmartRecruiters from './smartrecruiters';
-import Indeed from './indeed';
-import Workable from './workable';
-
-*/
-
-/*,
-  Lever,
-  JazzAPI,
-  JazzScrape,
-  SmartRecruiters,
-  Workable,
-  Indeed,
-  */
